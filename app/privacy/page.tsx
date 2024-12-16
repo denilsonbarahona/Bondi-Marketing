ac1b8cadad8402704653b3433045fb877d10bcc8
denilson <denilson@gmail.com>
2024-12-15 18:40:25 -0600

    feat: privacy

---
+++ app/privacy/page.tsx
@@ -0,0 +1,8 @@
+export default function Privacy() {
+  return (
+    <iframe
+      className="w-full h-screen"
+      src="https://www.termsfeed.com/live/84710197-fea1-457e-9370-5994a9e0438d"
+    />
+  );
+}
